8feae92f086ed7913a55a6dc5e910322cb2be07c
Andreas Nergaard <andreas.nergaard@cerpus.com>
2017-06-01 09:49:09 +0200

    HFP-266 Upgrade script fix Re-used the already updated minor version since it didn't have an upgrade script already and was just on master. Fixed upgrade script to correctly upgrade content

--- upgrades.js
+++ upgrades.js
@@ -3,19 +3,14 @@ var H5PUpgrades = H5PUpgrades || {};
 H5PUpgrades['H5P.ImageHotspotQuestion'] = (function ($) {
   return {
     1: {
-      7: {
+      6: {
         contentUpgrade: function (parameters, finished) {
-          if (parameters.hotspotSettings === undefined) {
-            parameters.hotspotSettings = {};
-          }
-
-          if (parameters.hotspotSettings.l10n === undefined) {
-            parameters.hotspotSettings.l10n = {
+          if (parameters.imageHotspotQuestion.hotspotSettings.l10n === undefined) {
+            parameters.imageHotspotQuestion.hotspotSettings.l10n = {
               retryText: 'Retry',
               closeText: 'Close'
             };
           }
-
           finished(null, parameters);
         }
       }
