4c3761bfc60eec89d279dfd26b7b34a756ca552e
Frode Petterson <frode.andre.petterson@gmail.com>
2021-05-31 16:01:15 +0200

    HFP-3201 Fix incomplete refactor from 2015
    One of the variables were never renamed

--- image-hotspot-question.js
+++ image-hotspot-question.js
@@ -318,7 +318,7 @@ H5P.ImageHotspotQuestion = (function ($, Question) {
    * @returns {boolean}
    */
   ImageHotspotQuestion.prototype.getAnswerGiven = function () {
-    return this.hotspotChosen;
+    return this.hotspotFeedback.hotspotChosen;
   };
 
   /**
